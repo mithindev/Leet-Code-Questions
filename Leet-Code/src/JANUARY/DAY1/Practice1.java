27fd06df25ec834addaea415861dc42d1f455781
mithindev <mithindev1@gmail.com>
2023-01-01 15:18:00 +0530

    updated

--- Leet-Code/src/JANUARY/DAY1/Practice1.java
+++ Leet-Code/src/JANUARY/DAY1/Practice1.java
@@ -3,27 +3,61 @@ import java.util.Arrays;
 
 public class Practice1 {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {2, 2, 3, 1};
+        boolean dup = false;
+        int[] ans = RmDuplicates(arr);
+        sort(ans);
+        System.out.println(Arrays.toString(ans));
 
+        System.out.println(ans.length);
+
+        if (ans.length == 2 || ans.length == 1) {
+            System.out.println(ans[ans.length-1]);
+
+        } else {
+            System.out.println(ans[ans.length-3]);
+
+        }
 
     }
-    private static void sort(int[] arr) {
+    static void sort(int[] arr) {
         for (int i = 0;i < arr.length;i++) {
             for (int j = 1; j < arr.length; j++) {
                 if (arr[j-1] > arr[j]) {
-                    swap(arr[j-1], arr[j]);
+//                    swap(arr[j-1], arr[j]);
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
     }
 
-    private static void swap(int a, int b) {
-        int tamp = a;
-        a = b;
-        b = a;
+    static int[] RmDuplicates(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (count(arr, arr[i]) == 1) {
+                count++;
+            }
+        }
+        int[] arr2 = new int[count];
+
+        for (int i = 0,k=0; i < arr.length; i++) {
+            if (count(arr, arr[i]) == 1) {
+                arr2[k] = arr[i];
+                k++;
+            }
+        }
+        return arr2;
     }
+
+
+
+//    static void swap(int a, int b) {
+//        int temp = a;
+//        a = b;
+//        b = temp;
+//    }
 
     static int[] RemoveElement(int[] arr, int index) {
         if (arr.length == 0 || index > arr.length) {
@@ -39,10 +73,20 @@ public class Practice1 {
         }
         return arr;
     }
-    private static int count(int[] arr, int target) {
+    static int count(int[] arr, int target) {
         int count = 0;
         for (int i = 0; i < arr.length;i++) {
             if (arr[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int unique(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (count(arr, arr[i]) == 1) {
                 count++;
             }
         }

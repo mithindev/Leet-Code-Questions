3de002f1ea76ed9601b0b657a8882b207fe2b6cc
mithindev <mithindev1@gmail.com>
2023-01-01 14:22:04 +0530

    updated

---
+++ Leet-Code/src/JANUARY/DAY1/Practice1.java
@@ -0,0 +1,7 @@
+package JANUARY.DAY1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Leet-Code/src/JANUARY/DAY2/demo.java
@@ -0,0 +1,7 @@
+package JANUARY.DAY2;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

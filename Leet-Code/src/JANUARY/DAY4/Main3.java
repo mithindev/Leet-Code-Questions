302dafc61efd8c94157eabacd8e88bf088f7a182
mithindev <mithindev1@gmail.com>
2023-01-04 07:27:19 +0530

    updated

--- Leet-Code/src/JANUARY/DAY4/Main3.java
+++ Leet-Code/src/JANUARY/DAY4/Main3.java
@@ -1,5 +1,43 @@
 package JANUARY.DAY4;
+import java.util.Arrays;
 
 public class Main3 {
+    public static void main(String[] args) {
+        int[] arr1 = {3, 2, 1};
+        sort(arr1);
+        int[] arr2 = {6, 5, 4};
+        sort(arr2);
+        int[] ans = merge(arr1, arr2);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static void sort(int[] arr) {
+        if (arr.length < 2 ) {
+            return;
+        }
+        for (int i = 0;i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j-1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+
+    static int[] merge(int[] arr1, int[] arr2) {
+        int[] ans = new int[arr1.length+arr2.length];
+        int k = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            ans[k] = arr1[i];
+            k++;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            ans[k] = arr2[i];
+            k++;
+        }
+        return ans;
+    }
     
 }

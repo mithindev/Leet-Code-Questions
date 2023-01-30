6865eb0fc919d09cdf5e1cdf86aa1b69754d85eb
mithindev <mithindev1@gmail.com>
2023-01-30 12:09:41 +0530

    updated

---
+++ Leet-Code/src/JANUARY/DAY30/Permutations.java
@@ -0,0 +1,19 @@
+package JANUARY.DAY30;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Permutations {
+    public static void main(String[] args) {
+
+    }
+
+    static List<List<Integer>> permute(int[] arr) {
+        List<Integer> p = new ArrayList<>();
+        List up = Arrays.asList(arr);
+
+
+    }
+}

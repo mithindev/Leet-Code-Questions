6c8c6be96f4a660cafed6ef5e25b67b87659f9ca
mithindev <mithindev1@gmail.com>
2023-02-05 09:50:32 +0530

    updated

--- Leet-Code/src/FEBRUARY/DAY5/IntersectionofTwoArrays.java
+++ Leet-Code/src/FEBRUARY/DAY5/IntersectionofTwoArrays.java
@@ -1,4 +1,20 @@
 package FEBRUARY.DAY5;
 
 public class IntersectionofTwoArrays {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int[] intersectionNum(int[] arr1, int[] arr2) {
+
+    }
 }

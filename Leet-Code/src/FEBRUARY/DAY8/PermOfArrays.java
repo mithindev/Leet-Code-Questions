3ebc8b987d0945ff7b5c65422ee6794cb9c5a38e
mithindev <mithindev1@gmail.com>
2023-02-08 10:16:49 +0530

    updated

---
+++ Leet-Code/src/FEBRUARY/DAY8/PermOfArrays.java
@@ -0,0 +1,36 @@
+package FEBRUARY.DAY8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PermOfArrays {
+    public static void main(String[] args) {
+
+    }
+
+    static List<List<Integer>> permute(int[] arr) {
+//        List<List<Integer>> outer = new ArrayList<>();
+//        for (int i = 1; i <= arr.length; i++) {
+//            int j = 0;
+//            while (j < i) {
+//                List<Integer> inner = new ArrayList<>();
+//
+//            }
+//
+//        }
+    }
+
+    static List<Integer> arrayToList (int[] arr) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i : arr) {
+            ans.add(i);
+        }
+        return ans;
+    }
+
+    static List<List<Integer>> permutationOf (List<Integer> p,) {
+
+    }
+}

2b86c8e1a8d8e7d42b3e336f6ab0c30511622b6a
mithindev <mithindev1@gmail.com>
2023-02-11 08:53:29 +0530

    updated

---
+++ Leet-Code/src/FEBRUARY/DAY11/SpiralMatrix.java
@@ -0,0 +1,29 @@
+package FEBRUARY.DAY11;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+
+    }
+
+//    public List<Integer> spiralOrder(int[][] matrix) {
+//        List<Integer> list = new ArrayList<>();
+//
+//        int i = 0;
+//        int j = 0;
+//
+//        while (j < matrix[0].length) {
+//            list.add(matrix[i][j]);
+//            j++;
+//        }
+//        i = j;
+//
+//
+//
+//
+//
+//    }
+}

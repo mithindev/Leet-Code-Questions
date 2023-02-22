0b4b6fb45d29ca128f91fed19a23468134c56423
mithindev <mithindev1@gmail.com>
2023-02-22 06:45:13 +0530

    updated

--- Leet-Code/src/FEBRUARY/DAY22/FindTriangularSumofanArray.java
+++ Leet-Code/src/FEBRUARY/DAY22/FindTriangularSumofanArray.java
@@ -7,52 +7,61 @@ import java.util.*;
 
 public class FindTriangularSumofanArray {
     public static void main(String[] args) {
-        int[] arr = {5};
+        int[] arr = {1, 2, 3, 4, 5};
         System.out.println(triangularSum(arr));
 
     }
 
+//    static int triangularSum(int[] arr) {
+//        List<Integer> temp = new ArrayList<>();
+//        temp = ArrtoList(arr);
+//        List<Integer> ans = new ArrayList<>();
+//
+//        if (temp.size() == 1) {
+//            return temp.get(0);
+//        }
+//
+//        for (int i = 0; i < temp.size()-1; i++) {
+//            int t = temp.get(i) + temp.get(i+1);
+//            ans.add(last(t));
+//        }
+//        if (ans.size() == 1) {
+//            return ans.get(0);
+//        } else {
+//            return triangularSum(listToArr(ans));
+//        }
+//
+//
+//
+//    }
+//
+//    static List<Integer> ArrtoList (int[] arr) {
+//        List<Integer> ans = new ArrayList<>();
+//        for (int i = 0; i < arr.length; i++) {
+//            ans.add(last(arr[i]));
+//        }
+//        return ans;
+//    }
+//
+//    static int[] listToArr (List<Integer> list) {
+//        int[] arr = new int[list.size()];
+//        for (int i = 0; i < list.size(); i++) {
+//            arr[i] = last(list.get(i));
+//        }
+//        return arr;
+//    }
+//
+//    static int last(int num) {
+//        return num%10;
+//    }
+
     static int triangularSum(int[] arr) {
-        List<Integer> temp = new ArrayList<>();
-        temp = ArrtoList(arr);
-        List<Integer> ans = new ArrayList<>();
-
-        if (temp.size() == 1) {
-            return temp.get(0);
+        for (int i = arr.length; i > 0; i--) {
+            for (int j = 0;j+1 < i; j++) {
+                arr[j] = (arr[j] + arr[j+1])%10;
+            }
         }
-
-        for (int i = 0; i < temp.size()-1; i++) {
-            int t = temp.get(i) + temp.get(i+1);
-            ans.add(last(t));
-        }
-        if (ans.size() == 1) {
-            return ans.get(0);
-        } else {
-            return triangularSum(listToArr(ans));
-        }
-
-
-
-    }
-
-    static List<Integer> ArrtoList (int[] arr) {
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            ans.add(last(arr[i]));
-        }
-        return ans;
-    }
-
-    static int[] listToArr (List<Integer> list) {
-        int[] arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            arr[i] = last(list.get(i));
-        }
-        return arr;
-    }
-
-    static int last(int num) {
-        return num%10;
+        return arr[0];
     }
 
 }

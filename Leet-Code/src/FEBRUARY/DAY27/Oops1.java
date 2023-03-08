9cd0d4d71d3734bfc41e7feafdc90cb601eb6354
mithindev <mithindev1@gmail.com>
2023-03-08 21:44:03 +0530

    updated

---
+++ Leet-Code/src/FEBRUARY/DAY27/Oops1.java
@@ -0,0 +1,11 @@
+package FEBRUARY.DAY27;
+
+public class Oops1 {
+    int num = 10;
+
+    public static void main(String[] args) {
+        Oops1 obj = new Oops1();
+        obj.num = 25;
+
+    }
+}

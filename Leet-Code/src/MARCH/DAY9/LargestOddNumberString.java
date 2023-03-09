40ccabf03fb9f78e98ca2dbd363ee472c1740786
mithindev <mithindev1@gmail.com>
2023-03-09 14:36:18 +0530

    updated

---
+++ Leet-Code/src/MARCH/DAY9/LargestOddNumberString.java
@@ -0,0 +1,16 @@
+package MARCH.DAY9;
+
+import java.lang.String;
+
+public class LargestOddNumberString {
+    public static void main(String[] args) {
+
+    }
+
+    public String largestOddNumber(String num) {
+        while ((int)(num.charAt(num.length() - 1))%2 == 0) {
+        }
+
+    }
+
+}

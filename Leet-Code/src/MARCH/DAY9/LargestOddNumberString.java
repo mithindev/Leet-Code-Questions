341bc3f52508d13f385e3795744cc4d6fb82f6de
mithindev <mithindev1@gmail.com>
2023-03-09 18:13:56 +0530

    updated

--- Leet-Code/src/MARCH/DAY9/LargestOddNumberString.java
+++ Leet-Code/src/MARCH/DAY9/LargestOddNumberString.java
@@ -4,13 +4,30 @@ import java.lang.String;
 
 public class LargestOddNumberString {
     public static void main(String[] args) {
-
+        System.out.println(largestOddNumber("35427"));
     }
 
-    public String largestOddNumber(String num) {
-        while ((int)(num.charAt(num.length() - 1))%2 == 0) {
-        }
+    static String largestOddNumber(String num) {
+        char[] str = num.toCharArray();
+        int i = num.length() - 1;
 
+        while (i >= 0) {
+            if ((int)(str[i])%2 == 0) {
+                i--;
+            } else {
+                return charToString (str, 0, i);
+            }
+        }
+        return "";
+    }
+
+    static String charToString (char[] str, int s, int e) {
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = s;i <= e; i++) {
+            ans.append(str[i]);
+        }
+        return ans.toString();
     }
 
 }

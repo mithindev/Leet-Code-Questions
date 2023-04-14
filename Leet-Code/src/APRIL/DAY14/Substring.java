0044c988c34bbab76124706a4da9936ed7ffb55b
mithindev1 <mithindev1@gmail.com>
2023-04-14 08:27:34 +0530

    updated

--- Leet-Code/src/APRIL/DAY14/Substring.java
+++ Leet-Code/src/APRIL/DAY14/Substring.java
@@ -1,8 +1,5 @@
 package APRIL.DAY14;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Substring {
     public static void main(String[] args) {
         System.out.println(longestPalindromeSubseq("cbbd"));
@@ -61,5 +58,61 @@ public class Substring {
 //        return ans;
 //    }
 
-    
+//    static int longestPalindromeSubseq(String s) {
+//        int len = s.length() - 1;
+//        return helper(s.toCharArray(), 0, len);
+//    }
+//
+//    static int helper(char[] str, int s, int e) {
+////        int l = str.length - 1;
+//
+//        if (s == e) {
+//            return 1;
+//        }
+//
+//        if (s+1 == e) {
+//            if (str[s] == str[e]) {
+//                return 2;
+//            } else {
+//                return 1;
+//            }
+//        }
+//
+//        if (str[s] == str[e]) {
+//            return helper(str, s + 1, e - 1) +2;
+//        }
+//
+//        return max(helper(str, s + 1, e), helper(str, s, e - 1));
+//    }
+//
+//    static int max (int num1, int num2) {
+//        return (num1 > num2) ? num1:num2;
+//    }
+
+    static int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int len = s.length();
+        
+        String revS = new StringBuilder(s).reverse().toString();
+        
+        int [][] matrix = new int[len + 1][len + 1];
+
+        for (int i = 0; i <= len; i++) {
+            for (int j = 0; j <= len; j++) {
+                if (i == 0 || j == 0) {
+                    matrix[i][j] = 0;
+                } else if (s.charAt(i - 1) == revS.charAt(j - 1)) {
+                     matrix[i][j] = matrix[i - 1][j-1] + 1;
+                } else {
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+                }
+            }
+            return matrix[len][len];
+        }
+    }
+
+
 }

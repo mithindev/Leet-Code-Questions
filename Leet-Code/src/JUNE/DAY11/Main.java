a544115d85d03ad44d7e6fbb46b3c2e489dc7ee4
mithindev <mithindev1@gmail.com>
2023-06-12 10:10:11 +0530

    jupdated

---
+++ Leet-Code/src/JUNE/DAY11/Main.java
@@ -0,0 +1,17 @@
+package JUNE.DAY11;
+
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    static LinkedList reverseKGroup(LinkedList head, int k) {
+
+    }
+
+    static void reverse (Node head)
+}

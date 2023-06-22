11dd6e3177b7bc31645e3d928aaae41f0b1438a3
mithindev <mithindev1@gmail.com>
2023-06-22 20:00:46 +0530

    MEDIUM

--- Leet-Code/src/JUNE/DAY22/Main.java
+++ Leet-Code/src/JUNE/DAY22/Main.java
@@ -2,30 +2,35 @@ package JUNE.DAY22;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(pivot(arr, 7));
+        int[] arr = {3, 1, 1};
+        System.out.println(pivot(arr));
     }
 
-    public int findMin(int[] arr) {
-        int s = pivot(arr, arr.length);
+//    public int findMin(int[] arr) {
+//        int s = pivot(arr);
+//
+//        return arr[s];
+//    }
 
-        return arr[s];
-    }
-
-    static int pivot(int[] arr, int len) {
+    static int pivot (int[] arr) {
         int s = 0;
-        int e = len - 1;
+        int e = arr.length-1;
 
         while (s < e) {
             int mid = s + (e - s)/2;
-
-            if (arr[mid] > arr[e]) {
-                s = mid + 1;
+            if (mid < e && arr[mid] > arr[mid + 1]) {
+                return mid + 1;
+            }
+            if (mid > s && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[s] >= arr[mid]) {
+                e = mid -1;
             } else {
-                e = mid;
+                s = mid + 1;
             }
         }
-        return s;
+        return -1;
     }
 
 }

0f99167494d5b8ef1ec49a72526247783a23b432
mithindev <mithindev1@gmail.com>
2023-09-19 10:06:18 +0530

    EASY

--- Leet-Code/src/SEPTEMBER/test.py
+++ Leet-Code/src/SEPTEMBER/test.py
@@ -1,15 +1,39 @@
-class Solution:
-    def GCD(self, nums: List[int]) -> int:
+from typing import List
 
-        m = max(nums)
-        n = min(nums)
-
-        for i in (range(m, 0, -1)):
-            if m % i == 0 and n % i ==0:
-                return i
+# def continuousSubarrays(arr: List[int]) -> int:
+#         l = len(arr)
+#         if (l == 0 or l == 1):
+#             return 0
         
-        return 1
+#         outer = []
 
-    def smallestEvenMultiple(self, n: int) -> int:
-        return (2 * n) // self.GCD([2, n])
+#         for i in range(l):
+#             s = 0
+#             e = s + i
+
+#             while (e < l):
+#                 inner = arr[s: e + 1]
+#                 s += 1
+#                 e += 1
+#                 outer.append(inner)
+            
+            
         
+#         return len(outer)
+
+def continuousSubarrays(arr: List[int]) -> int:
+        l = len(arr)
+        if (l == 0 or l == 1):
+            return 0
+        
+        count = 0  # Initialize the count of continuous subarrays
+
+        for i in range(l):
+            for j in range(i, l):
+                count += 1  # Increment count for each continuous subarray
+            
+        return count
+
+lst = [5, 4, 2, 4]
+        
+print(continuousSubarrays(lst))

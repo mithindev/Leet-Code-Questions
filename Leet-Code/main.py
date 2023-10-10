2f4662dbc817172986b3eb5916cf4a9f5ea42ba4
mithindev <mithindev1@gmail.com>
2023-10-10 16:31:30 +0530

    MEDIUM

--- Leet-Code/main.py
+++ Leet-Code/main.py
@@ -1,73 +1,74 @@
-# from typing import List
+# # from typing import List
 
-# def index(arr, target, l):
-#     ans = ""
-#     for i in range(len):
-#         if arr[i] == target:
-#             ans += str(i)
-#     return ans
+# # def index(arr, target, l):
+# #     ans = ""
+# #     for i in range(len):
+# #         if arr[i] == target:
+# #             ans += str(i)
+# #     return ans
 
-# def solver(s, i):
-#     ans = 0
-#     for x in s:
-#         ans += abs(int(x) - i)
-#     return ans
+# # def solver(s, i):
+# #     ans = 0
+# #     for x in s:
+# #         ans += abs(int(x) - i)
+# #     return ans
      
 
-# def distance(nums: List[int]) -> List[int]:
-#     ans = []
-#     l = len(nums)
-#     dict = {}
-#     for i in set(nums):
-#         dict[i] = index(nums, i)
+# # def distance(nums: List[int]) -> List[int]:
+# #     ans = []
+# #     l = len(nums)
+# #     dict = {}
+# #     for i in set(nums):
+# #         dict[i] = index(nums, i)
 
-#     for i in range(l):
-#         temp = nums[i]
-#         indeces = dict[temp]
-#         ans.append(solver(indeces, i))
-#     return ans
+# #     for i in range(l):
+# #         temp = nums[i]
+# #         indeces = dict[temp]
+# #         ans.append(solver(indeces, i))
+# #     return ans
         
 
 
 
 
-# # lst = [1, 1, 2, 3, 1, 1, 3]
-# # print(lst.index(1, 0, len(lst)))
+# # # lst = [1, 1, 2, 3, 1, 1, 3]
+# # # print(lst.index(1, 0, len(lst)))
 
-import sys
-from typing import List
+# import sys
+# from typing import List
 
-def minOperations(arr: List[int]) -> int:
+# def minOperations(arr: List[int]) -> int:
 
-    l = len(arr)
-    arr = list(set(arr))
+#     l = len(arr)
+#     arr = list(set(arr))
 
-    arr.sort()
-    ans = -sys.maxsize - 1
+#     arr.sort()
+#     ans = -sys.maxsize - 1
 
-    e = 0
-    for s in range(l):
-        while (e < len(arr) and arr[e] < arr[s] + l):
-            e += 1
+#     e = 0
+#     for s in range(l):
+#         while (e < len(arr) and arr[e] < arr[s] + l):
+#             e += 1
         
-        sw = e - s
-        ans = max(ans, sw)
+#         sw = e - s
+#         ans = max(ans, sw)
     
-    return l - ans
+#     return l - ans
 
 
-    # for i in arr:
-    #     sw = 0
+#     # for i in arr:
+#     #     sw = 0
             
-    #     if (i + l < arr[len(arr) - 1]):
-    #         for j in range (i, i + l):
-    #             if j in arr:
-    #                 sw += 1
+#     #     if (i + l < arr[len(arr) - 1]):
+#     #         for j in range (i, i + l):
+#     #             if j in arr:
+#     #                 sw += 1
 
-    #     if sw > ans:
-    #         ans = sw
+#     #     if sw > ans:
+#     #         ans = sw
         
-    # return l - ans
+#     # return l - ans
         
 
-print(minOperations([4,2,5,3]))
+# print(minOperations([4,2,5,3]))
+

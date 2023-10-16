36db5bebeb97701a065800de580425a524939392
mithindev <mithindev1@gmail.com>
2023-10-16 19:34:22 +0530

    MEDIUM

--- Leet-Code/main5.py
+++ Leet-Code/main5.py
@@ -1,24 +1,58 @@
+# from typing import List
+
+# def removeDuplicates(arr: List[int]) -> int:
+#         if len(arr) <= 2:
+#             return arr
+
+#         s = 0
+#         f = 0
+#         count = 0
+
+#         while f < len(arr):
+#             while arr[s] == arr[f]:
+#                 f += 1
+#                 count += 1            
+
+#             else:
+#                 while s < len(arr) and count != 2:
+#                      arr[s] = 'r'
+#                      count -= 1
+#                      s += 1
+
+#                 if count == 2:
+#                     s = f
+#                     count = 0
+        
+#         return arr
+
+# print(removeDuplicates([0,0,1,1,1,1,2,3,3, 3, 3]))
+
 from typing import List
+import sys
 
-def buildArray(target: List[int], n: int) -> List[str]:
-    stack = []
-    ans = []
+def removeDuplicates(arr: List[int]) -> int:
+        if len(arr) <= 2:
+            return len(arr)
 
-    pt = 0
+        count = 1
+        temp = len(arr)
 
-    for i in range(1, n + 1):
-        stack.append(i)
-        ans.append("Push")
-        if i != target[pt]:
-            stack.pop()
-            ans.append("Pop")
-        else:
-            pt += 1
+        for i in range(1, len(arr)):
+            if arr[i] == arr[i - 1]:
+                count += 1
+                if count >= 2:
+                    arr[i] = 'r'
+            else:
+                count = 1
+        
+        arr = [x for x in arr if x != 'r']
+        k = len(arr)
 
-        if pt == len(target):
-            break
+        for i in range(k, temp):
+            arr.append(0)
+        
+        print(arr)
+        
+        return k
 
-    return ans
-
-
-print(buildArray([1,3], 3))
+print(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3, 3, 3]))
